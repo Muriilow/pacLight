75a7c116b9ff256c442252b2cb4ff80250b972d1
Muriilo <87148235+Muriilow@users.noreply.github.com>
2026-04-25 20:04:33 -0300

    Initializing game logic, creating the csv base for the map and defining the rest of the types of messages

--- Message.h
+++ Message.h
@@ -6,7 +6,18 @@
 
 #define TYPE_ACK  0
 #define TYPE_NACK 1
-#define TYPE_DATA 3
+#define TYPE_VISUAL 2
+#define TYPE_INITIALIZING 3
+#define TYPE_DATA 4 
+#define TYPE_TXT 5
+#define TYPE_JPG 6 
+#define TYPE_MP4 7
+#define TYPE_RIGHT 10
+#define TYPE_LEFT 11
+#define TYPE_DOWN 12
+#define TYPE_UP 13
+#define TYPE_ERROR 15 
+#define TYPE_END 16 
 
 struct __attribute__((packed)) global_sequence {
     uint8_t value : 6;
